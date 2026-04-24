4185cc58ac13cb9fce589afedc31518b58fab0bd
mnc3403 <mnc3403@gmail.com>
2026-04-25 03:23:20 +0600

    initial commit

--- src/pages/hotelOwner.jsx/Dashboard.jsx
+++ src/pages/hotelOwner.jsx/Dashboard.jsx
@@ -46,8 +46,8 @@ const Dashboard = () => {
                   <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>{item.room.roomType}</td>
                   <td className='py-3 px-4 text-gray-700 border-t border-gray-300 text-center'> $ {item.totalPrice}</td>
                   <td className='py-3 px-4 text-gray-700 border-t border-gray-300 text-center'>
-                    <button className={`py-1 px-3 text-xs rounded-full max-auto ${item.ispaid ? 'bg-green-200 text-green-600' : 'bg-amber-200 text-yellow-600'}`}>
-                      {item.ispaid?'complete':'pending'}
+                    <button className={`py-1 px-3 text-xs rounded-full mx-auto ${item.isPaid ? 'bg-green-200 text-green-600' : 'bg-amber-200 text-yellow-600'}`}>
+                      {item.isPaid?'complete':'pending'}
                     </button>
 
                  </td>
